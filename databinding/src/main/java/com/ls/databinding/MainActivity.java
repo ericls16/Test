39331d2052c7542ccec15b9478e6f322c86f3c5a
cnlius <cnlius@139.com>
2016-09-30 15:00:40 +0800

    16.9.30

---
+++ databinding/src/main/java/com/ls/databinding/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ls.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ls.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_test:
+                Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
