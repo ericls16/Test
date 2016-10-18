ce47b895815eadb71845643f1b5aad0b95f85016
cnlius <cnlius@139.com>
2016-10-18 17:11:27 +0800

    grid divider

--- widget/src/main/java/com/ls/widget/widget/decoration/DividerGridItemDecoration.java
+++ widget/src/main/java/com/ls/widget/widget/decoration/DividerGridItemDecoration.java
@@ -76,16 +76,15 @@ public class DividerGridItemDecoration extends RecyclerView.ItemDecoration {
     private boolean isLastColum(RecyclerView parent, int pos, int spanCount, int childCount) {
         LayoutManager layoutManager = parent.getLayoutManager();
         if (layoutManager instanceof GridLayoutManager) {
+            // 如果是最后一列，则不需要绘制右边
             if ((pos + 1) % spanCount == 0) {
-                // 如果是最后一列，则不需要绘制右边
                 return true;
             }
         } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-            int orientation = ((StaggeredGridLayoutManager) layoutManager)
-                    .getOrientation();
+            int orientation = ((StaggeredGridLayoutManager) layoutManager).getOrientation();
             if (orientation == StaggeredGridLayoutManager.VERTICAL) {
+                // 如果是最后一列，则不需要绘制右边
                 if ((pos + 1) % spanCount == 0) {
-                    // 如果是最后一列，则不需要绘制右边
                     return true;
                 }
             } else {
