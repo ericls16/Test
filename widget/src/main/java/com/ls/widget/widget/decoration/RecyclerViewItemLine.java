cbf23deab07e4f1aa3fe8af41bab32659dd1d0b2
cnlius <cnlius@139.com>
2016-10-19 17:32:07 +0800

    hh

---
+++ widget/src/main/java/com/ls/widget/widget/decoration/RecyclerViewItemLine.java
@@ -0,0 +1,12 @@
+package com.ls.widget.widget.decoration;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by VIC1 on 2016/10/19.
+ */
+
+public class RecyclerViewItemLine extends RecyclerView.ItemDecoration {
+
+    
+}
