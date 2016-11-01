96e19aa1b7727fb6141a7330c49ddf12a7186288
cnlius <cnlius@139.com>
2016-11-01 16:52:20 +0800

    xxx

--- app/src/main/java/com/ls/app/CalcStepActivity.java
+++ app/src/main/java/com/ls/app/CalcStepActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,16 +18,33 @@ import android.widget.TextView;
 
 public class CalcStepActivity extends Activity implements View.OnClickListener {
 
-    private TextView txtResult;
-    private SensorManager sensorManager;
+    private TextView txtStepCount;
+    private TextView txtStepDetector;
+    private SensorManager mSensorManager;
+    private Sensor  mStepCount;
+    private Sensor  mStepDetector;
+    private int stepCount=0;
 
-    final SensorEventListener sensorListener = new SensorEventListener() {
+    private final SensorEventListener sensorListener = new SensorEventListener() {
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
+//            float x = event.values[0];
+//            float y = event.values[1];
+//            float z = event.values[2];
+
+            if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+                txtStepCount.setText(event.values[0] + "步");
+                Log.e("STEP","count="+event.values[0]);
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+                if (event.values[0] == 1.0) {
+                    stepCount++;
+                    txtStepDetector.setText(stepCount + "步");
+                    Log.e("STEP","detector-1="+event.values[0]);
+                }
+            }
         }
 
         @Override
@@ -39,17 +57,33 @@ public class CalcStepActivity extends Activity implements View.OnClickListener {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_step);
-        txtResult= (TextView) findViewById(R.id.txt_result);
+        txtStepCount= (TextView) findViewById(R.id.txt_step_count);
+        txtStepDetector= (TextView) findViewById(R.id.txt_step_detector);
         //传感器管理
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start:
-                sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
+//                mSensorManager.registerListener(sensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
+                stepCount=0;
+                txtStepDetector.setText(stepCount + "步");
+
+                mStepCount = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+                mStepDetector  = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+                mSensorManager.registerListener(sensorListener, mStepDetector, SensorManager.SENSOR_DELAY_FASTEST);
+                mSensorManager.registerListener(sensorListener, mStepCount, SensorManager.SENSOR_DELAY_FASTEST);
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(sensorListener, mStepDetector);
+        mSensorManager.unregisterListener(sensorListener, mStepCount);
     }
 }
