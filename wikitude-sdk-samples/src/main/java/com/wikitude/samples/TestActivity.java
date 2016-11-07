37b4c0c873a03bc32297afe8f4568f5cf207244c
cnlius <cnlius@139.com>
2016-11-07 18:10:39 +0800

    xxx

---
+++ wikitude-sdk-samples/src/main/java/com/wikitude/samples/TestActivity.java
@@ -0,0 +1,18 @@
+package com.wikitude.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.wikitude.sdksamples.R;
+
+/**
+ * Created by VIC1 on 2016/11/7.
+ */
+
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
