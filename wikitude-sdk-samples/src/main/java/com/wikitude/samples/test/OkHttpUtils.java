41c87475679a33aa47132bdc07431f66006aff28
cnlius <cnlius@139.com>
2016-11-10 18:07:54 +0800

    xxx

---
+++ wikitude-sdk-samples/src/main/java/com/wikitude/samples/test/OkHttpUtils.java
@@ -0,0 +1,37 @@
+package com.wikitude.samples.test;
+
+import com.zhy.http.okhttp.callback.FileCallBack;
+
+import java.io.File;
+
+import okhttp3.Call;
+
+/**
+ * Created by VIC1 on 2016/11/10.
+ */
+
+public class OkHttpUtils {
+
+    String fileDir="assets/files";
+    String fileName="temp";
+
+    public void downloadFile(String destFileDir, String destFileName) {
+        com.zhy.http.okhttp.OkHttpUtils
+                .get()
+                .url("http://www.lewei.online/android.jpg")
+                .build()
+                .execute(new FileCallBack("","") {
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(File response, int id) {
+
+                    }
+                });
+    }
+
+}
